a1381deb6b96f9ea5e778ba9da7a1e0c5ab4e032
shiro16 <shiro16@pepabo.com>
2021-05-01 13:01:59 +0900

    add scope script

---
+++ react/1/scope.js
@@ -0,0 +1,35 @@
+var n = 0;
+
+if (true) {
+    var n = 50;
+    var m = 100;
+    console.log(n);
+}
+
+console.log(n);
+console.log(m);
+
+console.log("------------");
+
+let a = 0;
+
+if (true) {
+    let a = 50;
+    let b = 100;
+    console.log(a);
+}
+console.log(a);
+// console.log(b);
+
+console.log("------------");
+
+const x = 0;
+
+if (true) {
+    const x = 50;
+    const y = 100;
+    console.log(x);
+} 
+
+console.log(x);
+console.log(y);
