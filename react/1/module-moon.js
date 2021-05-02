d7efda3d40b295ebf27dfecc39e4da38b37115c0
shiro16 <shiro16@pepabo.com>
2021-05-02 12:50:35 +0900

    use module

---
+++ react/1/module-moon.js
@@ -0,0 +1,8 @@
+const moon = {
+    modifier: 'prism',
+    transform() {
+        console.log(`Moon ${this.modifier}`);
+    }
+}
+
+module.exports = moon;
---
+++ react/1/module-venus.js
@@ -0,0 +1,8 @@
+exports.transform = function () {
+    console.log('Venus power');
+}
+
+const finish = function() {
+    console.log('finished');
+}
+exports.finish = finish;
