a02a9f8cb4e7823deedcd5e89ddcd00595aa539d
shiro16 <shiro16@pepabo.com>
2021-05-04 12:57:15 +0900

    import js

---
+++ react/1/ts/dts/main.ts
@@ -0,0 +1,6 @@
+import { transform, Brooch, CosmicCompact, CrisisCompact } from "./transform";
+
+transform();
+transform(new Brooch());
+transform(new CosmicCompact());
+transform(new CrisisCompact());
